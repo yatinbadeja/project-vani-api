983d7f7b416d5d57599309df890fdd25e0fc970d
JineshPrajapat <prajajaptjinesh585@gmail.com>
2025-04-22 17:06:54 +0530

    refactor:start.py

--- start.py
+++ start.py
@@ -1,5 +1,4 @@
 if __name__ == "__main__":
     import uvicorn
-    import os
-    port = int(os.environ.get("PORT", 8000)) 
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
+
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8010, reload=True)
