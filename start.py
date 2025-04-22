3e907d40965f913abd8f0fec1bff0826e80a8d37
JineshPrajapat <prajajaptjinesh585@gmail.com>
2025-04-22 16:12:54 +0530

    updated requirements.txt

--- start.py
+++ start.py
@@ -2,4 +2,4 @@ if __name__ == "__main__":
     import uvicorn
     import os
     port = int(os.environ.get("PORT", 8000)) 
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8010, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
