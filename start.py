d5e47d40d7ed81fcd793c875fe198f4575cc4b1d
JineshPrajapat <prajajaptjinesh585@gmail.com>
2025-04-24 15:23:31 +0530

    refactor:port updated

--- start.py
+++ start.py
@@ -5,8 +5,8 @@ print("🔥 start.py is executing...")
 if __name__ == "__main__":
     try:
         import uvicorn
-        port = int(os.environ.get("PORT", 8030))
+        port = int(os.environ.get("PORT", 8010))
         print(f"🚀 Starting server on port {port}...")
-        uvicorn.run("app.main:app", host="0.0.0.0", port=port)
+        uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
     except Exception as e:
         print(f"❌ Failed to start: {e}")
