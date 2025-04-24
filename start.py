af0dcd9b47aefa4ad9f8a5b9f88c1e2540eeebda
JineshPrajapat <prajajaptjinesh585@gmail.com>
2025-04-24 12:55:27 +0530

    Bug:deployment

--- start.py
+++ start.py
@@ -3,4 +3,5 @@ import os
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8030))
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
+    print(f"Starting server on port {port}...")
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
